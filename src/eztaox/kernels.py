ca4802038ab2085b078508d2c3ddd3d0c036493a
Weixiang Yu <wy73@drexel.edu>
2025-02-28 04:13:00 +0000

    feat: +multiband kernel, +lc format util

---
+++ src/eztaox/kernels.py
@@ -0,0 +1,16 @@
+"Kernels module"
+
+import jax.numpy as jnp
+import tinygp
+from tinygp.helpers import JAXArray
+
+
+@tinygp.helpers.dataclass
+class mb_kernel(tinygp.kernels.quasisep.Wrapper):
+    amplitudes: jnp.ndarray
+
+    def coord_to_sortable(self, X) -> JAXArray:
+        return X[0]
+
+    def observation_model(self, X) -> JAXArray:
+        return self.amplitudes[X[1]] * self.kernel.observation_model(X[0])
---
+++ src/eztaox/utils.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+import jax.numpy as jnp
+from tinygp.helpers import JAXArray
+
+
+def formatlc(
+    ts: dict[str, Any],
+    ys: dict[str, Any],
+    yerrs: dict[str, Any],
+    band_order: dict[str, int],
+) -> tuple[tuple[JAXArray, JAXArray], JAXArray, JAXArray]:
+    "Transform data in dictionary to TinyGP friendly format."
+
+    band_keys = band_order.keys()
+    tss = jnp.concat([ts[key] for key in band_keys])
+    yss = jnp.concat([ys[key] for key in band_keys])
+    yerrss = jnp.concat([yerrs[key] for key in band_keys])
+    band_idxs = jnp.concat(
+        [jnp.ones(len(ts[x])) * band_order[x] for x in band_keys]
+    ).astype(int)
+
+    return (tss, band_idxs), yss, yerrss**2
