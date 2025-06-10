e011f8c57a52adcefa6ab96ebad29a145a69ee47
Weixiang Yu <wy73@drexel.edu>
2025-06-10 21:07:37 +0000

    update mlp fitter func
    - need to update example for new initSampler

--- src/eztaox/fitter.py
+++ src/eztaox/fitter.py
@@ -7,18 +7,17 @@ import jax
 import jax.numpy as jnp
 import jaxopt
 import optax
+from numpyro.handlers import seed
 from tinygp.helpers import JAXArray
 
 from eztaox.models import MultiVarModel, UniVarModel
 
 
-def fit(
+def random_search(
     model: UniVarModel | MultiVarModel,
-    optimizer: optax.GradientTransformation,
     initSampler: Callable,
     prng_key: jax.random.PRNGKey,
     nSample: int,
-    nIter: int,
     nBest: int,
     jaxoptMethod: str = "SLSQP",
     batch_size: int = 1000,
@@ -28,50 +27,62 @@ def fit(
     def loss(params) -> JAXArray:
         return -model.log_prob(params)
 
-
-    def single_loop(params) -> tuple[dict[str, JAXArray], JAXArray]:
-        opt_state = optimizer.init(params)
-        for _ in range(nIter):
-            grads = jax.grad(loss)(params)
-            updates, opt_state = optimizer.update(grads, opt_state)
-            params = optax.apply_updates(params, updates)
-
-        return params, loss(params)
-
     # init samples
-    initSamples = initSampler(prng_key, nSample)
+    init_keys = jax.random.split(prng_key, int(nSample))
+    batched_samples = jax.vmap(lambda k: seed(initSampler, rng_seed=k)())(init_keys)
 
-    if nSample == 1:
-        print('Simple minimizer')
-        # jaxopt optimize
-        opt = jaxopt.ScipyMinimize(fun=loss, method=jaxoptMethod)
-        soln = opt.run(initSamples)
-        best_param = soln.params
-
-        return best_param, loss(best_param)
-
-    # adam loops
-    opt_params, losses = jax.lax.map(single_loop, initSamples, batch_size=batch_size)
+    # batched loss
+    losses = jax.lax.map(loss, batched_samples, batch_size=batch_size)
 
     # select top nBest
     loss_idx = jnp.argsort(losses)
     top_params = {}
-    for p in opt_params:
-        top_params[p] = opt_params[p][loss_idx[:nBest]]
+    for p in batched_samples:
+        top_params[p] = batched_samples[p][loss_idx[:nBest]]
 
     # convert from pytree to list of pytrees
-    list_of_parmas = [
+    list_of_params = [
         dict(zip(top_params.keys(), values)) for values in zip(*top_params.values())
     ]
 
     # jaxopt optimize
     opt = jaxopt.ScipyMinimize(fun=loss, method=jaxoptMethod)
     log_prob, param = [], []
-    for item in list_of_parmas:
+    for item in list_of_params:
         soln = opt.run(item)
-        # if soln.state.success is np.True_:
         log_prob.append(-soln.state.fun_val)
         param.append(soln.params)
     best_param = param[jnp.argmax(jnp.asarray(log_prob))]
 
     return best_param, max(log_prob)
+
+
+def simpleOptimizer(
+    model: UniVarModel | MultiVarModel,
+    optimizer: optax.GradientTransformation,
+    initSample: dict[str, JAXArray],
+    nStep: int,
+) -> tuple[
+    dict[str, JAXArray], tuple[dict[str, JAXArray], JAXArray, dict[str, JAXArray]]
+]:
+    @jax.jit
+    def loss(params) -> JAXArray:
+        return -model.log_prob(params)
+
+    param_hist, loss_hist, grad_hist = [], [], []
+    params = initSample.copy()
+    opt_state = optimizer.init(params)
+    for _ in range(nStep):
+        # compute loss, grad for current param & save to hist
+        val, grad = jax.value_and_grad(loss)(params)
+        param_hist.append(params)
+        loss_hist.append(val)
+        grad_hist.append(grad)
+
+        # update
+        updates, opt_state = optimizer.update(grad, opt_state)
+        params = optax.apply_updates(params, updates)
+
+    param_hist = jax.tree_util.tree_map(lambda *xs: jnp.stack(xs), *param_hist)
+    grad_hist = jax.tree_util.tree_map(lambda *xs: jnp.stack(xs), *grad_hist)
+    return params, (param_hist, jnp.asarray(loss_hist), grad_hist)
